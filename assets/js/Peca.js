737086a5c083cbae3c05b0c49ffac1b0c9a20f40
Jean Ferri <jeanferrimh@gmail.com>
2022-04-29 06:43:59 -0300

    29/04/2022

---
+++ assets/js/Peca.js
@@ -0,0 +1,6 @@
+
+export class Peca{
+      medidax = '90' 
+} 
+
+console.log('ola mundo')
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
+import { Peca } from ""
+
 var calcDivisaoPeca1 = ( medidaXchapa, medidaXpeca ) => medidaXchapa / medidaXpeca
 
 var calcDivisaoPeca2 = ( medidaYchapa, medidaYpeca ) => medidaYchapa / medidaYpeca
+
+
+console.log(Peca)
