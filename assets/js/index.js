2e19bfa4bfe7aade03971009c93ba459175fe3ed
Jean Ferri <jeanferrimh@gmail.com>
2022-05-05 13:09:28 -0300

    05/05/2022

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,8 +17,25 @@ var calcDivisaoPeca1 = ( medidaXchapa, medidaXpeca ) => medidaXchapa / medidaXpe
 
 var calcDivisaoPeca2 = ( medidaYchapa, medidaYpeca ) => medidaYchapa / medidaYpeca
 
+var medidasYChapa1000 =[]
+var medidasYChapa1200=[]
+
+var medidasXChapa = [1000, 1200]
+
 document.getElementById('botaoFormulario').onclick = function calcularTudo(){
     const peca1 = new Peca(document.getElementById('pecaX').value , document.getElementById('pecaY').value, document.getElementById('pesoSolid').value, document.getElementById('material').value, document.getElementById('espessura').value)
-    console.log(peca1)
+    if (peca1.espessura == 0.95){
+       medidasYChapa1000.push(1630, 1690 , 2010, 2420, 3000)
+       medidasYChapa1200.push(2100, 2625, 2781, 3000)
+       for(var i=0; i<medidasYChapa1000.length; i++){
+       var calculoPeca1 = calcDivisaoPeca1(1000,peca1.medidaXPeca)
+       var calculoPeca2 = calcDivisaoPeca2(medidasYChapa1000[i], peca1.medidaYPeca)
+       console.log(calculoPeca1, calculoPeca2)
+       
+       }
+    }
+
 }
+
+
 
