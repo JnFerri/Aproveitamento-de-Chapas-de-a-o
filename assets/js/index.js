e3efc8ad98ef72111ee3fce1a2988bd2fdb6582f
Jean Ferri <jeanferrimh@gmail.com>
2022-05-06 17:34:04 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,19 +24,32 @@ var medidasXChapa = [1000, 1200]
 
 document.getElementById('botaoFormulario').onclick = function calcularTudo(){
     const peca1 = new Peca(document.getElementById('pecaX').value , document.getElementById('pecaY').value, document.getElementById('pesoSolid').value, document.getElementById('material').value, document.getElementById('espessura').value)
+    
+    if(peca1.espessura == 0.5){
+        medidasYChapa1000.push(2990)
+    }
     if (peca1.espessura == 0.95){
        medidasYChapa1000.push(1620, 1680 , 2000, 2410, 2990)
-       medidasYChapa1200.push(2090, 2615, 2771, 2990)
+       medidasYChapa1200.push(2090, 2615, 2771, 2990)}
+    if (peca1.espessura == 1.25){
+        medidasYChapa1000.push(2410,2600,2990)
+        medidasYChapa1200.push(1565, 1665, 2490, 2850, 2990)
+    }
+    
        for(var i=0; i<medidasYChapa1000.length; i++){
        var calculoPeca1 = Math.round(calcDivisaoPeca(medidasXChapa[0],peca1.medidaXPeca)-0.5) * Math.round(calcDivisaoPeca(medidasYChapa1000[i], peca1.medidaYPeca)-0.5)
        var calculoPeca2 = Math.round(calcDivisaoPeca(medidasXChapa[0],peca1.medidaYPeca )-0.5) * Math.round(calcDivisaoPeca(medidasYChapa1000[i],peca1.medidaXPeca)-0.5)
-        var resultadoPecaHorizontal = `A seguinte chapa : ${medidasXChapa[0]} X ${medidasYChapa1000[i]} com peça na horizontal cabem o total de : ${calculoPeca1} Peças`
-        var resultadoPecaVertical = `A seguinte chapa : ${medidasXChapa[0]} X ${medidasYChapa1000[i]} com peça na vertical cabem o total de : ${calculoPeca2} Peças`
-        console.log(resultadoPecaHorizontal)
-       
+       var calculoPeca3 = Math.round(calcDivisaoPeca(medidasXChapa[1],peca1.medidaXPeca)-0.5) * Math.round(calcDivisaoPeca(medidasYChapa1200[i], peca1.medidaYPeca)-0.5)
+       var calculoPeca4 = Math.round(calcDivisaoPeca(medidasXChapa[1],peca1.medidaYPeca )-0.5) * Math.round(calcDivisaoPeca(medidasYChapa1200[i],peca1.medidaXPeca)-0.5)
+        var resultado1PecaHorizontal = `A seguinte chapa : ${medidasXChapa[0]} X ${medidasYChapa1000[i]} com peça na horizontal cabem o total de : ${calculoPeca1} Peças`
+        var resultado1PecaVertical = `A seguinte chapa : ${medidasXChapa[0]} X ${medidasYChapa1000[i]} com peça na vertical cabem o total de : ${calculoPeca2} Peças`
+        var resultado2PecaHorizontal= `A seguinte chapa : ${medidasXChapa[1]} X ${medidasYChapa1200[i]} com peça na horizontal cabem o total de : ${calculoPeca3} Peças` 
+        var resultado2PecaVertical = `A seguinte chapa : ${medidasXChapa[1]} X ${medidasYChapa1200[i]} com peça na horizontal cabem o total de : ${calculoPeca4} Peças`
+        console.log(resultado2PecaHorizontal)
     }
+    
 
-}
+
 
 
 }
