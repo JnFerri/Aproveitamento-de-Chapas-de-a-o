3f0269def255d77424660edaac2bebe27c75d62c
Jean Ferri <jeanferrimh@gmail.com>
2022-09-01 13:13:37 -0300

    01/09/2022

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,11 +2,7 @@
  export function publicarHeader(){
     var header = document.getElementById('header')
     header.innerHTML = `<div class="header__container">
-    <img class="header__imagem" src="assets/img/calculos.png" alt="imagem avioeste">
-   
-    <ul class="header__lista" type="none">
-        <li class="header__link"><a href="index.html">Home</a></li>
-    </ul>
+    <img class="header__imagem" src="assets/img/Calculos.png" alt="imagem avioeste">
 </div>`
 
 }
