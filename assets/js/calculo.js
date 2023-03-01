4ce2bf0a680bc8635f81dc030220c49fa1661521
Jean Ferri <jeanferrimh@gmail.com>
2023-03-01 13:02:52 -0300

    atualizado para mostrar quantidade do material como unidade

--- assets/js/calculo.js
+++ assets/js/calculo.js
@@ -6,10 +6,11 @@ var medidasYChapaB=[]
 
 
 var medidasXChapa = []
+var fatorMaterial = 0
 
 var calcDivisaoPeca = ( medidaChapa, medidaPeca ) => medidaChapa / medidaPeca
 
-var pesoChapa = (espessura, medidasXChapa, medidaYChapa) => (espessura * (medidasXChapa +10) * (medidaYChapa + 10) *0.00788) / 1000
+var pesoChapa = (espessura, medidasXChapa, medidaYChapa) => (espessura * (medidasXChapa +10) * (medidaYChapa + 10) * fatorMaterial) / 1000
 
 var pesoUnd = (pesodaChapa, numeroPecas) => pesodaChapa / numeroPecas
 var localResultado = document.getElementById('resultado')
@@ -22,67 +23,81 @@ export function calcularTudo(){
     if(peca1.espessura == 0.5 && peca1.material == 'galvanizado' ){
         medidasXChapa.push(990,1190)
         medidasYChapaA.push(2990)
+        fatorMaterial = 0.0078
     }
     if (peca1.espessura == 0.95){
         medidasXChapa.push(990,1190)
        medidasYChapaA.push(1620, 1680 , 2000, 2410, 2990)
        medidasYChapaB.push(2090, 2615, 2771, 2990)
+       fatorMaterial = 0.0078
     }
 
     if (peca1.espessura == 1.25 ){
         medidasXChapa.push(990,1190)
         medidasYChapaA.push(2410,2600,2990)
         medidasYChapaB.push(1565, 1665, 2490, 2850, 2990)
+        fatorMaterial = 0.0078
     }
     if(peca1.espessura == 1.95 ){
         medidasXChapa.push(990,1190)
         medidasYChapaA.push(1660, 2860, 2990)
         medidasYChapaB.push(1690, 2000, 2410, 2990)
+        fatorMaterial = 0.0078
     }
     if(peca1.espessura == 2.7 ){
         medidasXChapa.push(990,1190)
         medidasYChapaB.push(1990)
+        fatorMaterial = 0.0078
     }
     if(peca1.espessura == 0.8 && peca1.material == 'inox304'){
         medidasXChapa.push(1030, 1230)
         medidasYChapaA.push(1620, 2380, 2670, 2890)
         medidasYChapaB.push(2990)
+        fatorMaterial = 0.008
     }
     if(peca1.espessura == 1.2 && peca1.material == 'inox304'){
         medidasXChapa.push(1030, 1230)
         medidasYChapaA.push(2320, 2900)
         medidasYChapaB.push(1840, 2390, 2690, 2990)
+        fatorMaterial = 0.008
     }
     if(peca1.espessura == 1.5){
         medidasXChapa.push(1030, 1230)
         medidasYChapaA.push(2190)
         medidasYChapaB.push(2003, 2315,2990)
+        fatorMaterial = 0.008
     }
     if(peca1.espessura == 2 && peca1.material == 'inox304'){
         medidasXChapa.push(1030, 1230)
         medidasYChapaB.push(2990)
+        fatorMaterial = 0.008
     }
     if(peca1.espessura == 4){
         medidasXChapa.push(1030, 1230)
         medidasYChapaB.push(2990)
+        fatorMaterial = 0.008
     }
 
     if(peca1.espessura ==0.5 && peca1.material == 'inox430'){
         medidasXChapa.push(1030, 1230)
         medidasYChapaA.push(1830)
         medidasYChapaB.push(1830)
+        fatorMaterial = 0.008
     }
     if(peca1.espessura ==0.8 && peca1.material == 'inox430'){
         medidasXChapa.push(1030, 1230)
         medidasYChapaB.push(1875)
+        fatorMaterial = 0.008
     }
     if(peca1.espessura == 1.2 && peca1.material == 'inox430'){
         medidasXChapa.push(1030, 1230)
         medidasYChapaB.push(2990)
+        fatorMaterial = 0.008
     }
     if(peca1.espessura == 2 && peca1.material == 'inox430'){
         medidasXChapa.push(1030, 1230)
         medidasYChapaB.push(2990)
+        fatorMaterial = 0.008
     }
 
 
@@ -95,6 +110,7 @@ export function calcularTudo(){
         var pesoPeca1Vertical = pesoUnd (valorPesoChapaA, quantidadePeca2)
         var perda1Horizontal = ((pesoPeca1Horizontal / peca1.pesoSolid)-1) * 100
         var perda1Vertical = ((pesoPeca1Vertical / peca1.pesoSolid)-1) * 100
+        console.log(valorPesoChapaA)
         
 
 
@@ -108,6 +124,9 @@ export function calcularTudo(){
            </li>
            <li>
                <p class="resultado__quantidade">${quantidadePeca1} Peças</p>
+           </li>
+           <li>
+               <p class="resultado__quantidade">Valor em UND = ${(peca1.pesoSolid / valorPesoChapaA).toFixed(4)}</p>
            </li>
            <li>
                <p class="resultado__perca">${perda1Horizontal.toFixed(3)}%</p>
@@ -125,6 +144,9 @@ export function calcularTudo(){
            </li>
            <li>
                <p class="resultado__quantidade">${quantidadePeca2} Peças</p>
+           </li>
+           <li>
+               <p class="resultado__quantidade">Valor em UND = ${(peca1.pesoSolid / valorPesoChapaA).toFixed(4)}</p>
            </li>
            <li>
                <p class="resultado__perca">${perda1Vertical.toFixed(3)}%</p>
@@ -163,6 +185,9 @@ export function calcularTudo(){
                 <p class="resultado__quantidade">${quantidadePeca3} Peças</p>
             </li>
             <li>
+               <p class="resultado__quantidade">Valor em UND = ${(peca1.pesoSolid / valorPesoChapaB).toFixed(4)}</p>
+           </li>
+            <li>
                 <p class="resultado__perca">${perda2Horizontal.toFixed(3)}%</p>
             </li>
         </ul>
@@ -179,6 +204,9 @@ export function calcularTudo(){
     <li>
         <p class="resultado__quantidade">${quantidadePeca4} Peças</p>
     </li>
+    <li>
+               <p class="resultado__quantidade">Valor em UND = ${(peca1.pesoSolid / valorPesoChapaB).toFixed(4)}</p>
+           </li>
     <li>
         <p class="resultado__perca">${perda2Vertical.toFixed(3)}%</p>
     </li>
